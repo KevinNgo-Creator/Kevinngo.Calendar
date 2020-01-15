310292a4540181ff5b10148ac14e4a0ad9092a1b
Kevin Ngo <ltn8rd@mail.umkc.edu>
2020-01-15 01:14:11 -0600

    touch up

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,9 @@
-function renderTime(){
-    var mydate = new Date();
-    var year = mydate.getYear()
-        if(year < 1000){
-            year += 1900
-        }    
-    var day = mydate.getDay();
-    var month = mydate.getMonth();
-    var daym = mydate.getDate();
-    var dayarray = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-    var montharray = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
-// Date
-
-    var currentTime = new Date();
-    var h = currentTime.getHours();
-    var m = currentTime.getMinutes();
-    var s = currentTime.getSeconds();
-        if(h ==24){
-            h = 0;
-        } else if(h > 12){
-            h = h - 0;
-        }
-
-        if(h < 10){
-            h = "0" + h;
-        }
-
-        if(m < 10){
-            m = "0" + m;
-        }
-        
-        if(s < 10){
-            s = "0" + s;
-        }
-
-    var myClock = document.getElementById("clockDisplay")
-    myClock.textContent = "" +dayarray[day]+ " " +daym+ " " +montharray[month]+ " " +year+ " - " +h+ ":" +m+ ":" +s;
-    myClock.innerText = "" +dayarray[day]+ " " +daym+ " " +montharray[month]+ " " +year+ " - " +h+ ":" +m+ ":" +s;
-    setTimeout("renderTime()", 1000);
-
-}
-   
 // time
+var myClock = document.getElementById("clockDisplay")
+var format = moment().format('MMMM Do YYYY, h:mm a')
+$("#clockDisplay").html(format)
+console.log(format)
+
 var hours = moment().format("HHmm"); 
 var first = $("#0900").attr("dataTime")
 var firstT = parseInt(first)
@@ -199,6 +162,7 @@ function handleClick(e){
     localStorage.setItem("1700", input8)
 
 }
+
 window.onload = function(){
     var input11 = localStorage.getItem('0900');
     if (input11 !== null) $("#0900").val(input11)
